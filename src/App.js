dd02d291eea56df2e37920a1baddc6fc2a213373
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-11-20 20:55:23 +0300

    State UI'de goruntuleniyor.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {addOne, applyNumber} from "./actions";
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
   console.log(state);
-  
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -20,10 +20,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
 
-            <TotalDisplay value={0} />
+            <TotalDisplay value={state.total} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b>{state.operation} </span>
+              <span id="memory"><b>Memory:</b>{state.memory} </span>
             </div>
 
             <div className="row">
