4da32df61cfe79b9004ff4f902e231f0f021c6c9
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-11-20 21:04:39 +0300

    Event handler olusturuldu, addOne actionuna dispatch edildi.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
   console.log(state);
 
+  const clickHandler = () => {
+    dispatch(addOne());
+  }
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -33,7 +37,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} />
+              <CalcButton value={1} onClick = {clickHandler} />
               <CalcButton value={2} />
               <CalcButton value={3} />
             </div>
